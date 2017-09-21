9770efe62e15f04b0f7bf9e66671359440218980
Philip Basford <pjb1u12@soton.ac.uk>
2017-09-21 11:45:00 +0100

    Now uses logging and config file

--- client/MQTTClient.py
+++ client/MQTTClient.py
@@ -3,63 +3,91 @@
 """
 Created on Thu Aug 11 11:15:56 2016
 @author: sjj
+Modified 09/2017
+@author pjb
 """
 
-import datetime
-
-print 'Starting...'
+import logging
 import paho.mqtt.client as mqtt
+from configobj import ConfigObj
+
+DEFAULT_LOG_LEVEL = logging.WARN
+LOGGER = None
+CONFIG = None
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print "Connected with result code "+str(rc)
+    LOGGER.info("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
 
-    #https://www.thethingsnetwork.org/api/v0/gateways/B827EBFFFEE36EF8/
-    # umang node https://www.thethingsnetwork.org/api/v0/nodes/A870D662/
-    #sjj node https://www.thethingsnetwork.org/api/v0/nodes/4BE42396/
-    #ttn1 B827EBFFFEE36EF8 - Roof
-    # B827EBFFFE71AB02 - lanchester
-    # B827EBFFFEE96E6E - 3rd
-    #ttn2 = B827EBFFFE8E825E
-    #"longitude":-1.40481,"latitude":50.93717
 
-    client.subscribe("arduinoapp/devices/+/up", 0) #("+/devices/+/up" , 0 ) #("/70B3D57ED0000A45/devices/4BE42396/up", 0)
-    #client.subscribe("70B3D57ED0000A73/devices/+/up" , 0 ) #("70B3D57ED0000A45/devices/000000004BE42396/up" , 0 ) #("+/devices/+/up" , 0 ) #("/70B3D57ED0000A45/devices/4BE42396/up", 0)
+    client.subscribe(CONFIG.topic, CONFIG.qos)
+    LOGGER.info("TOPIC: " + str(CONFIG.topic))
+    LOGGER.info("QOS: " + str(CONFIG.qos))
 
-    print "here."
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print dir(msg)
-    print msg.topic+" "+str(msg.payload)
-    f = open('testing.json', 'a')
-    f.write(str(datetime.datetime.now()))
-    f.write(" " + str(msg.payload) + "\n")
-    f.close()
+    print msg.topic + " " + str(msg.payload)
 
 
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print "Subscribe" + mid
+    LOGGER.debug("Subscribe" + mid)
 
 
 def on_log(client, userdata, level, buf):
-    print str(level)+" "+str(buf)
+    LOGGER.debug(str(level) + " " + str(buf))
 
 
-client = mqtt.Client()
-client.username_pw_set('arduinoapp', "ttn-account-v2.1tSVEHu5apF8huYcS7QEP79cQUVm6cjWwMl3_GkNpyE")
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_subscribe_subscribe = on_subscribe
-client.on_log = on_log
-client.connect("eu.thethings.network", 1883, 10)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+def setup(config_file, log_level=DEFAULT_LOG_LEVEL):
+    global LOGGER
+    LOGGER = logging.getLogger("MQTT Client")
+    LOGGER.setLevel(log_level)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s')
+    global CONFIG
+    CONFIG = MqttConfig(config_file)
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_subscribe_subscribe = on_subscribe
+    client.on_log = on_log
+    LOGGER.debug("MQTT client created")
+    client.username_pw_set(CONFIG.user, CONFIG.password)
+    client.connect(CONFIG.server, CONFIG.port, CONFIG.keepalive)
+    LOGGER.info("MQTT Server: " + CONFIG.server)
+    LOGGER.info("MQTT Port: " + str(CONFIG.port))
+    LOGGER.info("MQTT Keepalive: " + str(CONFIG.keepalive))
+    LOGGER.info("MQTT Username: " + str(CONFIG.user))
+    LOGGER.info("MQTT Password: " + str(CONFIG.password))
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
+
+class MqttConfig(object):
+    def __init__(self, config_file):
+        try:
+            config = ConfigObj(config_file)
+            self.user = config["user"]
+            self.password = config["pass"]
+            self.server = config["server"]
+            self.port = int(config["port"])
+            self.keepalive = int(config["keepalive"])
+            self.topic = config["topic"]
+            self.qos = int(config["qos"])
+        except KeyError:
+            raise ConfigError("Invalid configuration given, missing a required option")
+        except ValueError:
+            raise ConfigError("Number expected but string given")
+
+class ConfigError(Exception):
+    """
+        Exception for use if the config file does not contain the required items
+    """
+    pass
